46dcd04cdef6de4e9ed4fef2bbb0660154c02232
Kepler452B <shaoyuanhong1234@gmail.com>
2023-10-12 11:41:22 +0800

    AddBom

---
+++ Software/OV_Watch_V2.3.2/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'OV_Watch_V2.3' 
+ * Target:  'OV_Watch_V2.3' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
